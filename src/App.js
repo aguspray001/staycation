bd27ce96f93da4e2e77ea9a1b2a4f387db883ab4
aguspray001 <aguspray001@gmail.com>
2021-10-26 09:17:20 +0700

    feat: add center header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import LandingPage from "pages/LandingPage";
 import React from "react";
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import "assets/scss/style.scss";
 import DetailPage from "pages/DetailPage";
+import Checkout from "pages/Checkout";
 
 function App() {
   return (
     <Router>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/properties/:id" component={DetailPage} />
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/properties/:id" component={DetailPage} />
+      <Route path="/checkout" component={Checkout} />
     </Router>
   );
 }
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,42 +1,60 @@
 import React from "react";
 import IconText from "./IconText";
 import Button from "elements/Button";
+import Fade from "react-reveal/Fade";
 
 export default function Header(props) {
   const getNavLinkClass = (path) => {
     return props.location.pathname === path ? " active" : "";
   };
+  if (props.isCentered) {
+    return (
+      <Fade>
+        <header className="spacing-sm">
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+              <Button className="brand-text-icon mx-auto" href="" type="link">
+                Stay<span className="text-gray-900">cation.</span>
+              </Button>
+            </nav>
+          </div>
+        </header>
+      </Fade>
+    );
+  }
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <IconText />
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            <li className={`nav-item${getNavLinkClass("/")}`}>
-              <Button className="nav-link" type="link" href="">
-                Home
-              </Button>
-            </li>
-            <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-              <Button className="nav-link" type="link" href="">
-                Browse By
-              </Button>
-            </li>
-            <li className={`nav-item${getNavLinkClass("/stories")}`}>
-              <Button className="nav-link" type="link" href="">
-                Stories
-              </Button>
-            </li>
-            <li className={`nav-item${getNavLinkClass("/agents")}`}>
-              <Button className="nav-link" type="link" href="">
-                Agents
-              </Button>
-            </li>
-          </ul>
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <IconText />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="">
+                    Browse By
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
-        </nav>
-      </div>
-    </header>
+      </header>
+    </Fade>
   );
 }
